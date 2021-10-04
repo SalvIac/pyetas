7b9d37612d649f7c3295d881ee8261e218dea93c
SalvIac <52127188+SalvIac@users.noreply.github.com>
2021-10-04 16:15:27 +0100

    [20211004] Restructuring
    4 Oct 2021

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pyetas',
+    version='0.0.1',
+    description='Python tool to calibrate ETAS - Salvatore Iacoletti',
+    author='Salvatore Iacoletti',
+    author_email='salvatore.iacoletti92@gmail.com',
+    url='https://github.com/SalvIac/pyetas',
+    packages=find_packages(),
+    zip_safe=False,
+    )
